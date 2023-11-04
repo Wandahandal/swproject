233436abd7079d970236cc7e9f421042e3426778
Wanda Handal <wanda.handal@gmail.com>
2023-11-04 22:05:09 +0100

    Scraping test to csv

--- scrapetest.py
+++ scrapetest.py
@@ -1,9 +1,23 @@
 import praw
-
-def test_reddit_scraping():
-    reddit = praw.Reddit(client_id='AUdenJ8dFGlyW0B5PBezew', client_secret='PMLVog-4HRSGXV9dUJ2JbLIPssgQ_Q', user_agent='scrapetest')
-    hot_posts = list(reddit.subreddit('Machinelearning').hot(limit=10))
-    assert hot_posts  # Check if hot posts are retrieved
-    for post in hot_posts:
-        print(post.title)
-
+import pandas as pd
+ 
+reddit_read_only = praw.Reddit(client_id="AUdenJ8dFGlyW0B5PBezew",client_secret="PMLVog-4HRSGXV9dUJ2JbLIPssgQ_Q",user_agent="scrapetest")        # your user agent
+ 
+# URL of the post
+url = "https://www.reddit.com/r/movies/comments/155ag1m/\official_discussion_oppenheimer_spoilers/"
+ 
+# Creating a submission object
+submission = reddit_read_only.submission(url=url)
+from praw.models import MoreComments
+ 
+post_comments = []
+ 
+for comment in submission.comments:
+    if type(comment) == MoreComments:
+        continue
+ 
+    post_comments.append(comment.body)
+ 
+# creating a dataframe
+comments_df = pd.DataFrame(post_comments, columns=['comment'])
+comments_df.to_csv("Comments on subreddit", index=True)
