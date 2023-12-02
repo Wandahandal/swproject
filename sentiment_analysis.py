bd0b17f18d23df1987a1105c09563a01fb0aae9a
Wanda Handal <wanda.handal@gmail.com>
2023-12-02 18:02:54 +0100

    all code

--- sentiment_analysis.py
+++ sentiment_analysis.py
@@ -1,30 +1,21 @@
 import pandas as pd
+import vaderSentiment
 from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
 from flair.models import TextClassifier
 from flair.data import Sentence
 
-def clean_data(data):
-    # Lowercase
-    data = data.lower()
-    # Removing URLs with a regular expression
-    data = re.sub(r'https?://\S+|www\.\S+', '', data)
-    # Remove Emails
-    data = re.sub('\S*@\S*\s?', '', data)
-    # Remove new line characters
-    data = re.sub('\s+', ' ', data)
-    # Remove single quotes
-    data = re.sub("\'", "", data)
-    data = re.sub(r'\d+', '', data)  # remove digits
-    data = re.sub(r'[^\w\s]', '', data)  # remove special characters
-    return data
 
-def get_vader_sentiment_scores(comment, vader_compound_scores):
+comments_df = pd.read_csv('Subreddit comments.csv', usecols=[1], names=['Comments'], sep=';', header=0)
+comments_df['Comments'].fillna('None', inplace=True)
+
+# Define functions to get sentiment scores
+def vader_sentiment_scores(comment):
     analyzer = SentimentIntensityAnalyzer()
     sentiment = analyzer.polarity_scores(comment)
     vader_compound_scores.append(sentiment['compound'])
     return sentiment
 
-def get_flair_sentiment_scores(comment, flair_count_scores):
+def flair_sentiment_scores(comment):
     classifier = TextClassifier.load('en-sentiment')
     sentence = Sentence(comment)
     classifier.predict(sentence)
@@ -33,29 +24,20 @@ def get_flair_sentiment_scores(comment, flair_count_scores):
     flair_count_scores.append(sentiment)
     return sentiment, score
 
-# Read CSV file into a DataFrame
-comments_df = pd.read_csv('Subreddit comments.csv', usecols=[1], names=['Comments'], sep=';', header=0)
-
-# Clean data
-comments_df['Comments'] = comments_df['Comments'].fillna('None').apply(clean_data)
-
-# Lists to store compound scores
+#define lists to store compund scores
 vader_compound_scores = []
 flair_count_scores = []
 
-# Apply sentiment analysis functions and store results in new columns
-comments_df['Vader sentiment scores'] = comments_df['Comments'].apply(lambda x: get_vader_sentiment_scores(x, vader_compound_scores))
-comments_df['Flair sentiment scores'] = comments_df['Comments'].apply(lambda x: get_flair_sentiment_scores(x, flair_count_scores))
+# apply the sentiment analysis function to the comments column and store the results in new columns
+comments_df['Vader sentiment scores'] = comments_df['Comments'].apply(vader_sentiment_scores)
+comments_df['Flair sentiment scores'] = comments_df['Comments'].apply(flair_sentiment_scores)
 
-# Calculate average Vader compound score
 average_score = sum(vader_compound_scores) / len(vader_compound_scores)
-comments_df.loc['Averages', 'Vader sentiment scores'] = average_score
+comments_df.loc['Averages','Vader sentiment scores'] = average_score
 print(f'Vader - the average value for the submitted post is {average_score}')
 
-# Count positive and negative Flair sentiment scores
 x, y = flair_count_scores.count('POSITIVE'), flair_count_scores.count('NEGATIVE')
-comments_df.loc['Averages', 'Flair sentiment scores'] = (f'n positives: {x}, n negatives: {y}')
+comments_df.loc['Averages','Flair sentiment scores'] = (f'n positives: {x}, n negatives: {y}')
 print(f'Flair - the positive and negative comments for the submitted post are, respectively: {x} positives, {y} negatives')
 
-# Save results to CSV file
 comments_df.to_csv("Comments sentiment analysis.csv", index=True, sep=';')
